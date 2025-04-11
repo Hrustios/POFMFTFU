01bf5b0c2c6bd704b560fcbf56603a3446c5d78c
Hrustios <andrejkulikov555@gmail.com>
2025-04-11 15:23:32 +0300

    Edit pz12-2 print

--- Kulikov Python/PZ12/script12-2.py
+++ Kulikov Python/PZ12/script12-2.py
@@ -4,7 +4,7 @@ def main():
     _str = input("Введите строку для обработки: \n")
     text = AlphaOut(_str)
     literals = [x for x in text]
-    print("\nБуквы из введённой строки:\n"," , ".join(literals))
+    print("\nБуквы из введённой строки:\n"," ".join(literals))
     
 def AlphaOut(_str):
     for x in _str:
