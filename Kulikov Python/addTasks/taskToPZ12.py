46be0905bba9c9ef587e204a97fdcbfdc1efa6d9
Hrustios <andrejkulikov555@gmail.com>
2025-04-12 17:47:43 +0300

    Update taskToPZ12.py

--- Kulikov Python/addTasks/taskToPZ12.py
+++ Kulikov Python/addTasks/taskToPZ12.py
@@ -1,12 +1,13 @@
-_str = list(map(str,input()))
-_str.remove('[')
-_str.remove(']')
-f = list(("".join(_str)).split(','))
-g = []
-print(f)
-for x in f:
-    int(x)
-    g.append(x)
-_m =g.pop(g.min)
-print(_m + g.min())
-    
+def main():
+    _str = list(map(str,input("Введите массив:\n")))
+    _str.remove('[')
+    _str.remove(']')
+    _list = list(("".join(_str)).split(','))
+    _int_list = list(map(int, _list))
+    _m = _int_list.pop(_int_list.index(min(_int_list)))
+    print(int(_m) + int(min(_int_list)))
+    print(_m)
+    print(min(_int_list))
+    print(_int_list)
+
+main()
